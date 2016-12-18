a641303eab36fcdad7e829022af95d28a2883e7c
Coto Cisternas <cotocisternas@gmail.com>
2016-12-18 11:18:04 -0300

    change influxdb::config::mode from fixnum to string

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,12 +4,12 @@
 #
 # Author: Coto Cisternas <cotocisternas@gmail.com>
 class influxdb::config inherits influxdb {
-  
+
   file { $influxdb::config_path:
     ensure  => file,
     owner   => 0,
     group   => 0,
-    mode    => 0644,
+    mode    => '0644',
     content => template('influxdb/influxdb.conf.erb'),
     notify  => Service[$influxdb::service_name],
     require => Package[$influxdb::package_name],
